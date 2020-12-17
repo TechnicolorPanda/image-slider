02de074a363a6ed9624e1729c42c4fa1c234a415
LorriAnn <LorriAnn7@att.net>
2020-12-16 21:43:18 -0500

    enabled image rotation after 5 seconds

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -6,12 +6,63 @@ const loadImageSlider = (
   const contentBox = contentSection;
 
   contentBox.innerHTML = `
-        <div id = 'picture'>
-        <div class = 'display-container'>
-          <img src='images/kyrra1.jpg' alt='Kyrra in Wonder Woman pose'>
-        </div>
+    <div class = 'display-container'>
+      <div class = 'slide'>
+        <img src = 'images/kyrra1.jpg' alt = 'Kyrra in Wonder Woman pose'>
       </div>
-      `;
+      <div class = 'slide'>
+        <img src = 'images/kyrra2.jpg' alt = 'Kyrra hiding behind a tree'>
+      </div>
+      <div class = 'slide'>
+        <img src = 'images/kyrra3.jpg' alt = 'Kyrra by brick wall'>
+      </div>
+      <div class = 'slide'>
+         <img src = 'images/kyrra 4.jpg' alt = 'Kyrra by yellow flowers'>
+      </div>
+      <div class = 'slide'>
+        <img src = 'images/kyrra5.jpg' alt = 'Kyrra by orange flowers'>
+      </div>
+    </div>
+  `;
+
+  let imageNumber = 0;
+  rotateImages(imageNumber);
 };
+
+function slideImages(imageNumber) {
+  console.log('slide images');
+  const displaySlide = document.getElementsByClassName('slide');
+  for (let i = 0; i < displaySlide.length; i++) {
+    if (imageNumber === i) {
+      displaySlide[i].style.display = 'block';
+    } else {
+      displaySlide[i].style.display = 'none';
+    }
+  }
+}
+
+// change image every 5 seconds
+
+function rotateImages(imageNumber) {
+  console.log('rotate slide');
+  const displaySlide = document.getElementsByClassName('slide');
+  console.log(displaySlide);
+  for (let i = 0; i < displaySlide.length - 1; i++) {
+  console.log(i);
+    setTimeout(() => {
+      slideImages(i);
+      if (i === displaySlide.length - 2) {
+        setTimeout(setImageNumber(), 5000);
+      }
+    }, i * 5000);
+  }
+}
+
+// set image number
+
+function setImageNumber(imageNumber) {
+  console.log('set image number');
+  //slideImages(imageNumber);
+}
 
 export { loadImageSlider };
